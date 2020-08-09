d4409407e6f72901a96296229a5078d77bdf33b4
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2020-08-09 15:08:41 -0300

    feat(author): Creates author model, table and factory

---
+++ app/Author.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Author Model.
+ */
+class Author extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'gender',
+        'country',
+    ];
+}
